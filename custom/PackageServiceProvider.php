6788202a6b39ad5cd22a8891cf55114bcafaa70c
covaleski <covaleski23@gmail.com>
2026-04-09 21:30:02 -0300

    feat: add the first service provider

---
+++ custom/PackageServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Covaleski\LaravelBootstrap5\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
