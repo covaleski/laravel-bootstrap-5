a62fd02498127b3497a0492834252e25b8e837b7
covaleski <covaleski23@gmail.com>
2026-06-14 13:50:15 -0300

    test: add basic tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,21 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Ensure directives create the correct HTML tags.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_directives_work(): void
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
+        $response->assertSeeHtml('<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" hash="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" as="style"/>');
+        $response->assertSeeHtml('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" hash="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"/>');
+        $response->assertSeeHtml('<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" crossorigin="anonymous" as="style"/>');
+        $response->assertSeeHtml('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" crossorigin="anonymous"/>');
+        $response->assertSeeHtml('<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" hash="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer="defer"></script>');
     }
 }
