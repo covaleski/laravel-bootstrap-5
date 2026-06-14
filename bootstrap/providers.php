555959b8bda010099df0a643192580757828a0fc
covaleski <covaleski23@gmail.com>
2026-06-14 13:19:59 -0300

    refactor: install Testbench

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    Covaleski\LaravelBootstrap5\Providers\PackageServiceProvider::class,
-    Covaleski\LaravelBootstrap5\Providers\BladeServiceProvider::class,
-    Covaleski\LaravelBootstrap5\Providers\HelperServiceProvider::class,
-];
---
+++ src/Providers/PackageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Covaleski\LaravelBootstrap5\Providers;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Path for Blade views.
+     */
+    protected string $path;
+
+    /**
+     * Create the service provider instance.
+     */
+    public function __construct(Application $app)
+    {
+        $this->path = dirname(dirname(__DIR__));
+        return parent::__construct($app);
+    }
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            "{$this->path}/config/bootstrap.php",
+            'bootstrap',
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            "{$this->path}/config/bootstrap.php" => config_path('bootstrap.php'),
+        ]);
+    }
+}
