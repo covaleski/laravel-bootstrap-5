abf610bf4b78a9f76a0c57f61cda6425227ec3d9
covaleski <covaleski23@gmail.com>
2026-06-14 13:35:11 -0300

    refactor: reduce the number of service providers

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -3,17 +3,20 @@
 namespace Covaleski\LaravelBootstrap5\Providers;
 
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades;
 use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
 {
     /**
-     * Path for Blade views.
+     * Package root path.
      */
     protected string $path;
 
     /**
      * Create the service provider instance.
+     *
+     * Adds the package vendor directory path for convenience.
      */
     public function __construct(Application $app)
     {
@@ -37,8 +40,46 @@ class PackageServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->publishes([
-            "{$this->path}/config/bootstrap.php" => config_path('bootstrap.php'),
-        ]);
+        $this->publishes(
+            [
+                "{$this->path}/config/bootstrap.php" => config_path('bootstrap.php'),
+                "{$this->path}/resources/views" => resource_path('views'),
+            ],
+            'laravel-bootstrap-5',
+        );
+        Facades\Blade::anonymousComponentPath(
+            "{$this->path}/resources/views/components",
+            'bs',
+        );
+        Facades\Blade::directive(
+            'bootstrap_css',
+            fn (string $expression) => "<link <?= (new \\Illuminate\\View\\ComponentAttributeBag())->merge(array_replace(config('bootstrap.css'), {$expression})) ?>/>",
+        );
+        Facades\Blade::directive(
+            'bootstrap_css_preload',
+            fn (string $expression) => "<link <?= (new \\Illuminate\\View\\ComponentAttributeBag())->merge(array_replace(config('bootstrap.css'), ['rel' => 'preload', 'as' => 'style'], {$expression})) ?>/>",
+        );
+        Facades\Blade::directive(
+            'bootstrap_icons',
+            fn (string $expression) => "<link <?= (new \\Illuminate\\View\\ComponentAttributeBag())->merge(array_replace(config('bootstrap.icons'), {$expression})) ?>/>",
+        );
+        Facades\Blade::directive(
+            'bootstrap_icons_preload',
+            fn (string $expression) => "<link <?= (new \\Illuminate\\View\\ComponentAttributeBag())->merge(array_replace(config('bootstrap.icons'), ['rel' => 'preload', 'as' => 'style'], {$expression})) ?>/>",
+        );
+        Facades\Blade::directive(
+            'bootstrap_js',
+            fn (string $expression) => "<script <?= (new \\Illuminate\\View\\ComponentAttributeBag())->merge(array_replace(config('bootstrap.js'), {$expression})) ?>></script>",
+        );
+        Facades\Blade::directive(
+            'variant',
+            fn (string $expression) => <<<PHP
+                <?php {$expression} = array_search(
+                    true,
+                    compact(config('bootstrap.variants.available')),
+                    true,
+                ) ?: config('bootstrap.variants.default'); ?>
+                PHP,
+        );
     }
 }
